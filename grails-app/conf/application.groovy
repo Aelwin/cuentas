14b3d8cbb6fbdcd02c81fb974c91d917518b0b17
unknown <AdmApl@E100153.desa.vitoria-gasteiz.net>
2016-09-08 08:59:00 +0200

    cambios conexion db

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -1,6 +1,19 @@
 environments {
   production {
     dataSource {
+      dbCreate = "create"
+      driverClassName = "org.postgresql.Driver"
+      dialect = org.hibernate.dialect.PostgreSQLDialect      
+      url = "jdbc:postgresql://fdptuejixcpmwl:ZOblCrvU1d6qwnrPlPc-fMDAFB@ec2-54-243-212-122.compute-1.amazonaws.com:5432/db1m382o2ljaem"
+      username = "fdptuejixcpmwl"
+      password = "ZOblCrvU1d6qwnrPlPc-fMDAFB"
+    }
+  }
+}
+
+/*environments {
+  production {
+    dataSource {
       dbCreate = "update"
       driverClassName = "org.postgresql.Driver"
       dialect = org.hibernate.dialect.PostgreSQLDialect
@@ -10,4 +23,4 @@ environments {
       url = "jdbc:postgresql://" + username + ":" + password + "@" + uri.host + ":" + uri.port + uri.path      
     }
   }
-}
+}*/
