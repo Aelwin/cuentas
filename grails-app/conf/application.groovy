85d3a9b5c4ee230df66f0e17af8bce9dcac9ef67
unknown <AdmApl@E100153.desa.vitoria-gasteiz.net>
2016-09-08 07:58:00 +0200

    conexion db

---
+++ grails-app/conf/application.groovy
@@ -0,0 +1,12 @@
+environments {
+  production {
+    dataSource {
+      dbCreate = "create"
+      driverClassName = "org.postgresql.Driver"
+      dialect = org.hibernate.dialect.PostgreSQLDialect      
+      url = "jdbc:postgresql://fdptuejixcpmwl:ZOblCrvU1d6qwnrPlPc-fMDAFB@ec2-54-243-212-122.compute-1.amazonaws.com:5432/db1m382o2ljaem"
+      username = "fdptuejixcpmwl"
+      password = "ZOblCrvU1d6qwnrPlPc-fMDAFB"
+    }
+  }
+}
