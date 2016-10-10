89a077a5046bb19e5facd04605b71239741ea290
unknown <AdmApl@E100153.desa.vitoria-gasteiz.net>
2016-10-10 08:52:14 +0200

    autenticacion

--- grails-app/controllers/jlr/NombreCuentaController.groovy
+++ grails-app/controllers/jlr/NombreCuentaController.groovy
@@ -2,7 +2,9 @@ package jlr
 
 import static org.springframework.http.HttpStatus.*
 import grails.transaction.Transactional
+import grails.plugin.springsecurity.annotation.Secured
 
+@Secured('ROLE_ADMIN')
 @Transactional(readOnly = true)
 class NombreCuentaController {
 
