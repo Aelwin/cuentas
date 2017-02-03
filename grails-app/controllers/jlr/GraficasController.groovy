599ad19c9b2eb9c322e9a529b0da3935baa72adf
Aelwin <juanlopez633@gmail.com>
2017-02-03 07:55:14 +0100

    cambiado como devuelve el json

--- grails-app/controllers/jlr/GraficasController.groovy
+++ grails-app/controllers/jlr/GraficasController.groovy
@@ -2,14 +2,19 @@ package jlr
 
 class GraficasController {
 
+    static responseFormats = ['json', 'html']
+    static defaultAction = "gastosPorTipo"
+
     def busquedaService
     def chartService
 
     def gastosPorTipo() { }
 
     def rellenarDatos() {
-        println "Entrando a rellenarDatos con nombreCuenta: $params.nombreCuenta"
-        render(contentType: "application/json") { chartService.parsearDatosChart(busquedaService.gastosPorNombreAlMes(params.nombreCuenta)) }
+        def datos = chartService.parsearDatosChart(busquedaService.gastosPorNombreAlMes(params.nombreCuenta))
+        println "Datos: $datos"
+        //render(contentType: "application/json") { datos }
+        respond datos
     }
 
 }
