686d3b7d40fb8ba4829b47212dadea3bc8b5975b
Aelwin <juanlopez633@gmail.com>
2017-02-03 07:54:21 +0100

    quitados mensajes de log

--- grails-app/services/jlr/ChartService.groovy
+++ grails-app/services/jlr/ChartService.groovy
@@ -3,7 +3,6 @@ package jlr
 class ChartService {
 
     def parsearDatosChart(datos) {
-        println "Entrando a parsearDatosChart"
         def etiquetas = []
         def valores = []
         datos.each { anyo, datosAnyo ->
@@ -12,8 +11,6 @@ class ChartService {
                 valores << datosMes
             }
         }
-        println "Etiquetas: $etiquetas"
-        println "Valores: $valores"
         ["etiquetas": etiquetas, "datos": valores]
     }
 }
