2320928f0f8a80f4a7deb713d194a5e786c89c29
Aelwin <juanlopez633@gmail.com>
2018-06-04 09:02:50 +0200

    quitada la anotación Transactional a nivel de controlador

--- grails-app/controllers/jlr/CuentaController.groovy
+++ grails-app/controllers/jlr/CuentaController.groovy
@@ -5,7 +5,6 @@ import grails.transaction.Transactional
 import grails.plugin.springsecurity.annotation.Secured
 
 @Secured('ROLE_ADMIN')
-@Transactional(readOnly = true)
 class CuentaController {
 
     static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
